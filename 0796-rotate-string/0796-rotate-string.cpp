d212cbb13bc8d7662fdf759cee94cc6354f4f873
TheHamzaCode <styleshamza@gmail.com>
2026-08-08 01:50:26 +0530

    Time: 0 ms (100%), Space: 8.1 MB (64.75%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
-    bool rotateString(string A, string B) {                
-    return A.size() == B.size() && (A + A).find(B) != string::npos;
-}    
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < goal.length(); i++) {
+            bool rotationPossible = true;
+
+            for (int j = 0; j < s.length(); j++) {
+                if (goal[(i + j) % goal.length()] != s[j]) {
+                    rotationPossible = false;
+                    break;
+                }
+            }
+
+            if (rotationPossible) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 };
